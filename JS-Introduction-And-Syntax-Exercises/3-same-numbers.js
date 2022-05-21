37a61db49f4c83e1c4692fbd1b2e8cfa4a0a857d
Lyubomir Tasev <78428295+LyubomirTasev@users.noreply.github.com>
2022-05-21 17:34:05 +0300

    Updated file

--- JS-Introduction-And-Syntax-Exercises/3-same-numbers.js
+++ JS-Introduction-And-Syntax-Exercises/3-same-numbers.js
@@ -1,12 +1,12 @@
 function solve(input) {
-    let num = input.toString(); // парсваме инпута в стринг, за да го обходим в цикъл
+    let num = input.toString(); 
     let flag = true;
     let sum = 0;
     let digitToCompare = num[0];
 
     for (let i = 0; i < num.length; i++) {
         
-        sum += Number(num[i]); // парсваме стринга към число, за да го добавим към сумата OR +num[i]
+        sum += Number(num[i]); 
 
         if (num[i] !== digitToCompare) {
             flag = false;
