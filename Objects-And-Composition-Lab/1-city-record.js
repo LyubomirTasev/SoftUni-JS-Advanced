936a93e57ec8bdd1170c42dc48050078a043bf1b
Lyubomir Tasev <lubomir.tasev@outlook.com>
2022-05-21 17:44:32 +0300

    Initial commit

---
+++ Objects-And-Composition-Lab/1-city-record.js
@@ -0,0 +1,13 @@
+function solve(name, population, treasury){
+    let cityRecord = {
+        name, 
+        population,
+        treasury
+    }
+
+    return cityRecord;
+}
+
+console.log('Tortuga',
+    7000,
+    15000);
---
+++ Objects-And-Composition-Lab/2-town-population.js
@@ -0,0 +1,31 @@
+function solve(input){
+    let towns = {};
+    
+    for (const info of input) {
+        let infoArr = info.split(' <-> ');
+        let town = infoArr[0];
+        let population = Number(infoArr[1]);
+
+        if(!towns[town]){
+            towns[town] = 0;
+        }
+        towns[town] += population;
+        
+        // if(towns[town]){
+        //     towns[town] += population;
+        // } else {
+        //     towns[town] = population;
+        // }
+        
+    }
+
+    for (const town in towns) {
+        console.log(`${town} : ${towns[town]}`);
+    }
+}
+
+solve(['Istanbul <-> 100000',
+'Honk Kong <-> 2100004',
+'Jerusalem <-> 2352344',
+'Mexico City <-> 23401925',
+'Istanbul <-> 1000']);
